38275f343bab59c53c529790d458c9579685c74d
cherjiosclear <slopez0286@gmail.com>
2020-01-30 19:24:23 +0000

    Adding cities2

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -10,4 +10,23 @@ model = "City"
 parent_model = "State"
 
 
+@app_views.route("/states/<state_id>/cities", strict_slashes=False,
+                 methods=["GET"])
+def get_cities(state_id):
+    """GET /state api route"""
+    return get_models(parent_model, state_id, "cities")
+
+
+@app_views.route("/cities/<city_id>", methods=["GET"])
+def get_city(city_id):
+    """GET /city api route"""
+    return get_model(model, city_id)
+
+
+@app_views.route("/cities/<city_id>", methods=["DELETE"])
+def delete_city(city_id):
+    """DELETE /city api route"""
+    return delete_model(model, city_id)
+
+
 
